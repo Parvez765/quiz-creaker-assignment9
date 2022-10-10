b9b7dd6795ca99a5c1c264da985841e7f980188c
Pervez <pervezhossain1998@gmail.com>
2022-10-11 01:39:58 +0600

    final blog question added

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -12,6 +12,10 @@ const Blog = () => {
             <div>
                  <h1>How Does Context Api Work?</h1>
                 <h2>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative<br></br> to "prop drilling" or moving props from grandparent to child to parent, and so on.</h2>
+            </div><br></br>
+            <div>
+                 <h1> Write Few Lines About use href hook</h1>
+                <h2>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated.<br></br> It can be used to access a DOM element directly.useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.</h2>
             </div>
         </div>
         
