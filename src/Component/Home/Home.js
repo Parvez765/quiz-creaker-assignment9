544527b3725f217c0070456431b7e4f3fe21085a
Pervez <pervezhossain1998@gmail.com>
2022-10-10 21:25:57 +0600

    Header Section Added and Responsivness Added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import HeaderImage from "../../images/HeaderImage-01.png"
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
-            <div>This is Home</div>
+        <div className='headerContent'>
+            <div className='headerText'>
+                <h1>Lets Learn <br></br>Technology With Fun</h1>
+                <div className='navBtn'>
+                    <Link to="/classes">Our Classes</Link>
+                </div>
+            </div>
+            <div className='headerImage'>
+                <img src={HeaderImage} alt="" />
+            </div>
         </div>
     );
 };
