fcecdbe21e1380be6866ed784aa8b06ddf592895
Pervez <pervezhossain1998@gmail.com>
2022-10-11 00:32:27 +0600

    SweetAlert Added

--- src/Component/Quiz/Quiz.js
+++ src/Component/Quiz/Quiz.js
@@ -6,16 +6,16 @@ const Quiz = ({ quiz }) => {
     const { question, options, id, correctAnswer } = quiz
     console.log(quiz)
     
-    const quizAnswer = () => {
-        
-    }
+    
     return (
         <div className='questionContainer'>
             <h3>{question}</h3>
-            <div onClick={()=> quizAnswer()} className='quizAnswer'>
+            <div className='quizAnswer'>
                 {
                     options.map(option => <QuizOption
-                        option = {option}
+                        option={option}
+                        correctAnswer={correctAnswer}
+                      
                     ></QuizOption>)
                }
             </div>
--- src/Component/QuizOption/QuizOption.js
+++ src/Component/QuizOption/QuizOption.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Swal from 'sweetalert2'
+import "./QuizOption.css"
 
-const QuizOption = ({ option }) => {
+
+const QuizOption = ({ option, correctAnswer }) => {
+    
+    const quizAnswer = (option) => {
+        if (option === correctAnswer) {
+            Swal.fire(
+                'Good job!',
+                'The Answer Is Right!',
+                'success'
+            )
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'The Answer Is wrong!',
+                footer: 'Please Try again'
+            })
+        }
+        
+    }
    
     return (
-        <div>
-            <h4>{option}</h4>
+        <div onClick={() => quizAnswer (option)}>
+            <h4 className='quizOption'>{option}</h4>
         </div>
     );
 };
